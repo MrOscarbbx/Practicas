99fffa8a3e7d18fa06320419437d1ec4705cabc7
MrOscarbbx <79319705+MrOscarbbx@users.noreply.github.com>
2021-02-26 09:53:56 -0500

    BINGO

--- src/mx/com/softgame/poo1game/prueba/PruebaPersonaje.java
+++ src/mx/com/softgame/poo1game/prueba/PruebaPersonaje.java
@@ -9,34 +9,32 @@ public class PruebaPersonaje{
 		Personaje per03=new Personaje("Wendy");
 		Personaje per04=new Personaje("Lesly");
 		for (int i=0;i<3 ;i++ ) {
-			int ed=(int)(Math.random()*250)
-			if (setEdad(ed)){
+			int ed=(int)(Math.random()*250);
+			if (per01.setEdad(ed)){
 				per01.setEdad(ed);
 				System.out.println(per01.getDetalle()+"se modifico la edad");
 			}else{
 				System.out.println(per01.getDetalle()+"sin se modifico la edad"+per01.getEdad());
 			}
-			int ed=(int)(Math.random()*250)
-			if (setEdad(ed)){
-				per02.setEdad(ed);
+			int ed=(int)(Math.random()*250);
+			if (per02.setEdad(ed)){
 				System.out.println(per02.getDetalle()+"se modifico la edad");
 			}else{
 				System.out.println(per02.getDetalle()+"sin se modifico la edad"+per02.getEdad());
 			}
-			}
-			int ed=(int)(Math.random()*250)
+			int ed=(int)(Math.random()*250);
 			if (per03.setEdad(ed)){
-				per03.setEdad(ed);
-				System.out.println(per03.getDetalle()+"se modifico la edad");;
+				System.out.println(per03.getDetalle()+"se modifico la edad");
 			}else{
 				System.out.println(per03.getDetalle()+"sin se modifico la edad"+per03.getEdad());
 			}
-			int ed=(int)(Math.random()*250)
+			int ed=(int)(Math.random()*250);
 			do{
-				if (per) {
-					
-				}
-			}
+				if (per04.setEdad(ed)==150) {
+					System.out.println("***BINGO***");
+				}while(per04.setEdad(ed));
+			System.out.println(per04.getDetalle())
 		}
 	}
+}
 //
