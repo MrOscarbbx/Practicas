44b66b0871266b2e2c9e8d6e85ff23dac906230f
MrOscarbbx <79319705+MrOscarbbx@users.noreply.github.com>
2021-02-26 09:40:59 -0500

    Actualizacion

--- src/mx/com/softgame/poo1game/prueba/PruebaPersonaje.java
+++ src/mx/com/softgame/poo1game/prueba/PruebaPersonaje.java
@@ -12,13 +12,31 @@ public class PruebaPersonaje{
 			int ed=(int)(Math.random()*250)
 			if (setEdad(ed)){
 				per01.setEdad(ed);
+				System.out.println(per01.getDetalle()+"se modifico la edad");
+			}else{
+				System.out.println(per01.getDetalle()+"sin se modifico la edad"+per01.getEdad());
 			}
 			int ed=(int)(Math.random()*250)
 			if (setEdad(ed)){
 				per02.setEdad(ed);
+				System.out.println(per02.getDetalle()+"se modifico la edad");
+			}else{
+				System.out.println(per02.getDetalle()+"sin se modifico la edad"+per02.getEdad());
+			}
 			}
 			int ed=(int)(Math.random()*250)
-			if (setEdad(ed)){
+			if (per03.setEdad(ed)){
 				per03.setEdad(ed);
-}
+				System.out.println(per03.getDetalle()+"se modifico la edad");;
+			}else{
+				System.out.println(per03.getDetalle()+"sin se modifico la edad"+per03.getEdad());
+			}
+			int ed=(int)(Math.random()*250)
+			do{
+				if (per) {
+					
+				}
+			}
+		}
+	}
 //
