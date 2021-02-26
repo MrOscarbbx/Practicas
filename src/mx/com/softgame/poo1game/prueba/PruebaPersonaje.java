eb915836ffb0a646afb24bae929efcc9e61e77d7
MrOscarbbx <79319705+MrOscarbbx@users.noreply.github.com>
2021-02-26 09:17:56 -0500

    Practica07
    Estructuras de control

--- src/mx/com/softgame/poo1game/prueba/PruebaPersonaje.java
+++ src/mx/com/softgame/poo1game/prueba/PruebaPersonaje.java
@@ -1,40 +1,24 @@
+//Inicio 7:45
 package mx.com.softgame.poo1game.prueba;
 import mx.com.softgame.poo1game.personajes.Personaje;
 
 public class PruebaPersonaje{
 	public static void main (String[] args){
-		Personaje per01=new Personaje("Aline");
-		per01.setEdad(200);
+		Personaje per01=new Personaje("Omar");
 		Personaje per02=new Personaje("Erick");
-		per02.setEdad(10);
-		Personaje per03=new Personaje("Alan");
-		per03.setEdad(1);
+		Personaje per03=new Personaje("Wendy");
 		Personaje per04=new Personaje("Lesly");
-		per04.setEdad(-10);
-		Personaje per05=new Personaje("Brian");
-		per05.setEdad(500);
-		System.out.println(per01.getDetalle());
-		System.out.println(per02.getDetalle());
-		System.out.println(per03.getDetalle());
-		System.out.println(per04.getDetalle());
-		System.out.println(per05.getDetalle());
-		Personaje per06=per05;
-		Personaje per07=per04;
-		Personaje per08=per03;
-		Personaje per09=per02;
-		Personaje per10=per01;
-		per06.setNombre("Yosselin");
-		per08.setNombre("Jose Eduardo");
-		per10.setNombre("Ana");
-		int random1=(int)(Math.random()*301);
-		int random2=(int)(Math.random()*301);
-		per07.setEdad(random1);
-		per09.setEdad(random2);
-		System.out.println(per01.getDetalle());
-		System.out.println(per02.getDetalle());
-		System.out.println(per03.getDetalle());
-		System.out.println(per04.getDetalle());
-		System.out.println(per05.getDetalle());
-	}
+		for (int i=0;i<3 ;i++ ) {
+			int ed=(int)(Math.random()*250)
+			if (setEdad(ed)){
+				per01.setEdad(ed);
+			}
+			int ed=(int)(Math.random()*250)
+			if (setEdad(ed)){
+				per02.setEdad(ed);
+			}
+			int ed=(int)(Math.random()*250)
+			if (setEdad(ed)){
+				per03.setEdad(ed);
 }
 //
