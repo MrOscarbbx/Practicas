1999aa0072c468be5b2b4fa509233050a19180ed
MrOscarbbx <79319705+MrOscarbbx@users.noreply.github.com>
2021-02-26 10:21:52 -0500

    Termine
    Practica 07 Terminada

--- src/mx/com/softgame/poo1game/prueba/PruebaPersonaje.java
+++ src/mx/com/softgame/poo1game/prueba/PruebaPersonaje.java
@@ -8,35 +8,42 @@ public class PruebaPersonaje{
 		Personaje per02=new Personaje("Erick");
 		Personaje per03=new Personaje("Wendy");
 		Personaje per04=new Personaje("Lesly");
+		outfor:
 		for (int i=0;i<3 ;i++ ) {
+			
 			int ed=(int)(Math.random()*250);
 			if (per01.setEdad(ed)){
 				per01.setEdad(ed);
-				System.out.println(per01.getDetalle()+"se modifico la edad");
+				System.out.println(per01.getDetalle()+" se modifico la edad");
 			}else{
-				System.out.println(per01.getDetalle()+"sin se modifico la edad"+per01.getEdad());
+				System.out.println(per01.getDetalle()+" sin modificacion en la edad a "+ed);
 			}
-			int ed=(int)(Math.random()*250);
+			
+
+			ed=(int)(Math.random()*250);
 			if (per02.setEdad(ed)){
-				System.out.println(per02.getDetalle()+"se modifico la edad");
+				System.out.println(per02.getDetalle()+" se modifico la edad");
 			}else{
-				System.out.println(per02.getDetalle()+"sin se modifico la edad"+per02.getEdad());
+				System.out.println(per02.getDetalle()+" sin modificacion en la edad a "+ed);
 			}
-			int ed=(int)(Math.random()*250);
+			
+
+			ed=(int)(Math.random()*250);
 			if (per03.setEdad(ed)){
-				System.out.println(per03.getDetalle()+"se modifico la edad");
+				System.out.println(per03.getDetalle()+" se modifico la edad");
 			}else{
-				System.out.println(per03.getDetalle()+"sin se modifico la edad"+per03.getEdad());
+				System.out.println(per03.getDetalle()+" sin modificacion en la edad a "+ed);
 			}
-			int ed=(int)(Math.random()*250);
+			
+
 			do{
-				if (per04.setEdad(ed)==150) {
+				ed=(int)(Math.random()*250);
+				if (ed==150) {
 					System.out.println("***BINGO***");
-					break outfor;
+					continue outfor;
 				}
 			}while(per04.setEdad(ed));
 				System.out.println(per04.getDetalle());
-			outfor:
 		}
 	}
 }
