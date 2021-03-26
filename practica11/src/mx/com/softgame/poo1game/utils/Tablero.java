73484f97027c669d931917b4151fb17eb1218375
MrOscarbbx <79319705+MrOscarbbx@users.noreply.github.com>
2021-03-26 10:33:13 -0400

    Modificación de Tablero
    Se están agregando los métodos a Tablero
    8:33

--- practica11/src/mx/com/softgame/poo1game/utils/Tablero.java
+++ practica11/src/mx/com/softgame/poo1game/utils/Tablero.java
@@ -1,1 +1,20 @@
-public
+package mx.com.softgame.poo1game.utils;
+
+public class Tablero{
+
+	public static final int MAX_LINE=10;
+	private static int idx=-1;
+	private static Personaje[] personajes=new Personaje[MAX_LINE];
+
+	private Tablero(){
+	}
+
+	public static boolean addPersonaje(Personaje p){
+		if (idx>=0&&idx<MAX_LINE) {
+			personajes[idx]=p;
+			idx++;
+			return true;
+		}
+		return false;
+	}
+}
