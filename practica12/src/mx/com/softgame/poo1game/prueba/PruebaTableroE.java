da7f44755ff6f5d73c1adf81f203df2e9cf30e1e
MrOscarbbx <79319705+MrOscarbbx@users.noreply.github.com>
2021-04-16 08:39:37 -0400

    Primera salida
    Codigo hasta la primera salida de solo una excepción
    Paso 8
    8:39

--- practica12/src/mx/com/softgame/poo1game/prueba/PruebaTableroE.java
+++ practica12/src/mx/com/softgame/poo1game/prueba/PruebaTableroE.java
@@ -3,6 +3,7 @@ import mx.com.softgame.poo1game.personajes.malos.Zombie;
 import mx.com.softgame.poo1game.personajes.buenos.Planta;
 import mx.com.softgame.poo1game.personajes.Personaje;
 import  mx.com.softgame.poo1game.utils.Tablero;
+import  mx.com.softgame.poo1game.utils.TheException;
 
 public class PruebaTableroE{
 	public static void main(String[] args) {
@@ -20,17 +21,24 @@ public class PruebaTableroE{
 			new Zombie("Erick",20,false),
 			new Planta("Aline",40)
 		};
-
+		try{
 		for (Personaje p:arr) {
-			Tablero.addPersonaje();
+			Tablero.addPersonaje(p);
 		}
 		Tablero.delPersonaje();
 		/* COMENTARIO 1:
 		error: unreported exception TheException; must be caught or declared to be thrown
 		-El error sale porque no hemos declarado los metodos para lanzar las excepciones
+			COMENTARIO 2:
+		-Solo sale una linea de salida ya que el try lanza la excepcion inicial, y ya no ejecuta
+			lo demas
 		*/
 		Tablero.showAll();
 		for (int i=0; i<15 ;i++) {
+			Tablero.delPersonaje();
+			}
+		}catch(TheException e){
+			System.out.println(e+" "+e.getPoss());
 		}
 	}
 }
