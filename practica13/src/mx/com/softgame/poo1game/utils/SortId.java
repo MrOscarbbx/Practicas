07b025c5800facf353e326c57e94741e98bfde1a
MrOscarbbx <79319705+MrOscarbbx@users.noreply.github.com>
2021-04-23 09:23:47 -0400

    Termine
    Termine la practica 13
    9:23
    Succesful

--- practica13/src/mx/com/softgame/poo1game/utils/SortId.java
+++ practica13/src/mx/com/softgame/poo1game/utils/SortId.java
@@ -1,9 +1,9 @@
 package mx.com.softgame.poo1game.utils;
 import mx.com.softgame.poo1game.personajes.Personaje;
 import java.util.*;
-public class SortId implements Comparator {
-	public int compare(Object o1,Object o2){
-		int com=((Personaje)o1).getId()-((Personaje)o2).getId();
+public class SortId implements Comparator<Personaje> {
+	public int compare(Personaje o1,Personaje o2){
+		int com=(o1.getId()-o2.getId());
 		if (com==0) {
 			return 0;
 		}else if (com<1) {
