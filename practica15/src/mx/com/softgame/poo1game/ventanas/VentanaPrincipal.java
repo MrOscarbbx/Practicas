3d908dca32db6e127e126df53543dd13242c1909
MrOscarbbx <79319705+MrOscarbbx@users.noreply.github.com>
2021-05-07 09:39:44 -0400

    Arreglos
    Arreglos De ventana
    Paso 6
    9:39

--- practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -4,19 +4,40 @@ import java.awt.*;
 
 public class VentanaPrincipal{
 	JFrame f;
-	Jlable lblFile;
+	JLabel lblFile;
 	JTextField txtFile;
 	JButton btnOpen;
 	JTextArea txtContenido;
-	Jlable lblN;
-	Jlable lblLeidos;
+	JLabel lblN;
+	JLabel lblLeidos;
 	JButton btnExit;
 	public VentanaPrincipal(){
 		f= new JFrame("Practica 14");
-		lblFile= new JLable("Nombre de archivo");
+		lblFile= new JLabel("Nombre de archivo");
 		txtFile= new JTextField(20);
 		btnOpen= new JButton("Abrir archivo");
 		txtContenido= new JTextArea(30,40);
-		lblN=new 
+		lblN=new JLabel("CERO");
+		btnExit=new JButton("Salir");
+		lblLeidos= new JLabel("Caracteres Leidos");
+	}
+
+	public void initComponents(){
+		f.setLayout(new FlowLayout());
+		f.add(lblFile);
+		f.add(txtFile);
+		f.add(btnOpen);
+		f.add(txtContenido);
+		f.add(lblLeidos);
+		f.add(lblN);
+		f.add(btnExit);
+		f.setSize(550, 440);
+		f.pack();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+	public static void main(String[] args) {
+		VentanaPrincipal v= new VentanaPrincipal();
+		v.initComponents();
 	}
 }
