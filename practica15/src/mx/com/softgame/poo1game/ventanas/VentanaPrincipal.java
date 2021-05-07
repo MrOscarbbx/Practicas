a2e027d8d2a31bcc08112d29c05a55bf18498e35
MrOscarbbx <79319705+MrOscarbbx@users.noreply.github.com>
2021-05-07 12:11:27 -0400

    Termine
    Listo 12:11

--- practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -1,7 +1,7 @@
 package mx.com.softgame.poo1game.ventanas;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
+import java.io.*;
 import java.awt.event.*;
 
 public class VentanaPrincipal{
@@ -19,7 +19,7 @@ public class VentanaPrincipal{
 		txtFile= new JTextField(20);
 		btnOpen= new JButton("Abrir archivo");
 		txtContenido= new JTextArea(30,40);
-		lblN=new JLabel("CERO");
+		lblN=new JLabel("0");
 		btnExit=new JButton("Salir");
 		lblLeidos= new JLabel("Caracteres Leidos");
 	}
@@ -63,8 +63,40 @@ public class VentanaPrincipal{
 			JOptionPane.showMessageDialog(null,"No Existe");
 			return;	
 		}
-		if (archivo.isFile()) {
-				getContenido();
-		}else{getList();}
+		if (archivo.isFile()){
+			getContenido(archivo);
+		}
+		else if (archivo.isDirectory()){
+			getList(archivo);
+		}
+	}
+
+	public void getContenido(File arch){
+		String linea;
+		try{
+			FileReader input = new FileReader(arch);
+			BufferedReader bufInput = new BufferedReader(input);
+			linea = bufInput.readLine();
+			int count=Integer.parseInt(lblN.getText());
+			while ( linea != null ) {
+				count+=linea.length();
+				txtContenido.append(linea+"\n");
+				linea = bufInput.readLine();
+			}
+			int total=Integer.parseInt(lblN.getText())+count;
+			lblN.setText(String.valueOf(total));
+			bufInput.close();
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	public void getList(File arch){
+		String[] lista = arch.list();
+		int count=Integer.parseInt(lblN.getText());
+		for (String archivo :lista ) {
+			count+=archivo.length();
+			txtContenido.append(archivo+"\n");
+		}
+		lblN.setText(String.valueOf(Integer.parseInt(lblN.getText())+count));
 	}
 }
