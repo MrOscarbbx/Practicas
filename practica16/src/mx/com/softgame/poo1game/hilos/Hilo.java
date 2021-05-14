29eba7554cbb6c507fa1da60fe147c1c65ac468d
MrOscarbbx <79319705+MrOscarbbx@users.noreply.github.com>
2021-05-14 08:17:58 -0400

    Paso 2 Terminado
    8:17

--- practica16/src/mx/com/softgame/poo1game/hilos/Hilo.java
+++ practica16/src/mx/com/softgame/poo1game/hilos/Hilo.java
@@ -1,9 +1,28 @@
-
+package mx.com.softgame.poo1game.hilos;
+import javax.swing.JLabel;
+import java.io.*;
 public class Hilo extends Thread{
-	String Nombre;
-	JLable etiqueta;
-	public Hilo(String nombre,JLable etiqueta){
+	String nombre;
+	JLabel etiqueta;
+	public Hilo(String nombre,JLabel etiqueta){
 		this.nombre=nombre;
 		this.etiqueta=etiqueta;
+	}
+	public void run(){
+		String str="";
+		for (int i=0;i<10 ;i++ ) {
+			str=etiqueta.getText();
+			char c=str.charAt(str.length());
+			str+=c+"";
+			etiqueta.setText(str);
+			int n=(int)(Math.random()*1000);
+			try{
+				this.sleep(n);
+			}catch(InterruptedException e){
+				System.err.println(e);
+			}
+		}
+		str+="HECHO";
+		etiqueta.setText(str);
 	}
 }
